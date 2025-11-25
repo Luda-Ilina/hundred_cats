81ed3f79b3f695e6b79b5b9005f33c953842d04f
Liudmila Ilina <liudmilla.ilina@gmail.com>
2025-11-25 16:48:16 +0100

    100 cats

--- download_cats.py
+++ download_cats.py
@@ -10,44 +10,66 @@ CATS_DIR = CATS_DIR = BASE_DIR / 'cats'
 
 
 def get_new_image_url():
+    # Отправить GET-запрос и получить JSON-ответ от API.
     response = requests.get(URL).json()
+    # Извлечь URL нулевого элемента списка из ответа.
     random_cat = response[0].get('url')
+    # Вернуть URL изображения.
     return random_cat
 
 
 def download_file(url):
+    # Получить имя файла из URL: разделить строку по символу '/' 
+    # и взять последний элемент.
+    # Например, из 'http://example.com/file.txt' будет получено 'file.txt'.
     filename = url.split('/')[-1]
+    
+    # Отправить GET-запрос по указанному URL и получить ответ.
     response = requests.get(url)
     response.raise_for_status()  # Проверка, что запрос выполнен успешно
+    
+    # Открыть файл для записи в двоичном режиме 
+    # в указанной директории CATS_DIR
+    # и записать содержимое ответа в файл.
     with open(CATS_DIR / filename, 'wb') as file:
         file.write(response.content)
 
 
 def download_new_cat_image():
+    # Получить URL нового изображения, вызвав функцию get_new_image_url().
     url = get_new_image_url()
-    download_file(url)
+    # Скачать файл-картинку по полученному URL, вызвав функцию download_file().
+    download_file(url) 
 
 
 def create_dir(dir_name):
+    # Создать директорию, если она не существует.
     os.makedirs(dir_name, exist_ok=True)
 
 
 def list_dir(dir_name):
+    # Вывести содержимое директории, переданной в качестве аргумента dir_name.
     print(
+        # Получить список файлов и папок в указанной директории.
         *os.listdir(dir_name),
+        # Вывести каждый элемент списка на новой строке.
         sep='\n'
     )
 
 
 def main():
+    # Создать директорию CATS_DIR, если она ещё не существует.
     create_dir(CATS_DIR)
+    
+    # Запустить цикл, который повторяется 100 раз.
     for _ in range(100):
+        # Скачать новое изображение и сохранить его в CATS_DIR.
         download_new_cat_image()
 
 
 if __name__ == '__main__':
-    # start_time = datetime.now()
+    start_time = datetime.now()
     main()
-    # end_time = datetime.now()
-    # print(f'Время выполнения программы: {end_time - start_time}.')
+    end_time = datetime.now()
+    print(f'Время выполнения программы: {end_time - start_time}.')
     list_dir(CATS_DIR)
